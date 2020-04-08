fc37a4d4f778b12671495aa3fc4d0de2350548b2
gulivero1773 <gulivero1773@gmail.com>
2020-04-08 22:34:06 +0300

    write project with second time

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,32 +1,64 @@
-import FluentSQLite
+import FluentPostgreSQL
 import Vapor
+import Leaf
+
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
-    // Register providers first
-    try services.register(FluentSQLiteProvider())
-
-    // Register routes to the router
+    /// Register providers first
+    try services.register(FluentPostgreSQLProvider())
+    
+    
+    /// Register routes to the router
     let router = EngineRouter.default()
     try routes(router)
     services.register(router, as: Router.self)
-
-    // Register middleware
+    
+    /// Register middleware
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
-    // middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
+    /// middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
-
-    // Configure a SQLite database
-    let sqlite = try SQLiteDatabase(storage: .memory)
-
-    // Register the configured SQLite database to the database config.
+    
+    // Configure a database
     var databases = DatabasesConfig()
-    databases.add(database: sqlite, as: .sqlite)
+    let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
+    let databaseName: String
+    let databasePort: Int
+    if (env == .testing) {
+        databaseName = "vapor-test"
+        if let testPort = Environment.get("DATABASE_PORT") {
+            databasePort = Int(testPort) ?? 5433
+        } else {
+            databasePort = 5433
+        }
+    } else {
+        databaseName = "vapor"
+        databasePort = 5432
+    }
+    
+    let databaseConfig = PostgreSQLDatabaseConfig(
+        hostname: hostname,
+        port: databasePort,
+        username: "vapor",
+        database: databaseName,
+        password: "password")
+    let database = PostgreSQLDatabase(config: databaseConfig)
+    databases.add(database: database, as: .psql)
     services.register(databases)
-
-    // Configure migrations
+    
     var migrations = MigrationConfig()
-    migrations.add(model: Todo.self, database: .sqlite)
+    migrations.add(model: User.self, database: DatabaseIdentifier<User.Database>.psql)
+    migrations.add(model: Acronym.self, database: DatabaseIdentifier<Acronym.Database>.psql)
+    migrations.add(model: Category.self, database: DatabaseIdentifier<Category.Database>.psql)
+    migrations.add(model: AcronymCategoryPivot.self, database: DatabaseIdentifier<AcronymCategoryPivot.Database>.psql)
+    
     services.register(migrations)
+    
+    var commandConfig = CommandConfig.default()
+    commandConfig.useFluentCommands()
+    services.register(commandConfig)
+    
+    try services.register(LeafProvider())
+    config.prefer(LeafRenderer.self, for: ViewRenderer.self)
 }
