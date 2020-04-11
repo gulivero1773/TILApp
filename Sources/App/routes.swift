4af7a8ce8c4e27bdd4302c099beae86e38850976
gulivero1773 <gulivero1773@gmail.com>
2020-04-11 13:29:28 +0300

    sing in with google

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,16 +3,19 @@ import Fluent
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
-  let acronymsController = AcronymsController()
-  try router.register(collection: acronymsController)
-
-  let usersController = UsersController()
-  try router.register(collection: usersController)
-
-  let categoriesController = CategoriesController()
-  try router.register(collection: categoriesController)
-
-  let websiteController = WebsiteController()
-  try router.register(collection: websiteController)
+    let acronymsController = AcronymsController()
+    try router.register(collection: acronymsController)
+    
+    let usersController = UsersController()
+    try router.register(collection: usersController)
+    
+    let categoriesController = CategoriesController()
+    try router.register(collection: categoriesController)
+    
+    let websiteController = WebsiteController()
+    try router.register(collection: websiteController)
+    
+    let imperialController = ImperialController()
+    try router.register(collection: imperialController)
 }
 
